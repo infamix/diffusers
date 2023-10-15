d5cb1777d052ca84238e3ef045e75d688019eb09
infamix <34374618+infamix@users.noreply.github.com>
2023-10-16 00:25:43 +0200

    final VQModel tiling fix

--- src/diffusers/models/vq_model.py
+++ src/diffusers/models/vq_model.py
@@ -176,6 +176,18 @@ class VQModel(ModelMixin, ConfigMixin):
 
         return DecoderOutput(sample=dec)
 
+    def blend_v(self, a, b, blend_extent):
+        blend_extent = min(a.shape[2], b.shape[2], blend_extent)
+        for y in range(blend_extent):
+            b[:, :, y, :] = a[:, :, -blend_extent + y, :] * (1 - y / blend_extent) + b[:, :, y, :] * (y / blend_extent)
+        return b
+
+    def blend_h(self, a, b, blend_extent):
+        blend_extent = min(a.shape[3], b.shape[3], blend_extent)
+        for x in range(blend_extent):
+            b[:, :, :, x] = a[:, :, :, -blend_extent + x] * (1 - x / blend_extent) + b[:, :, :, x] * (x / blend_extent)
+        return b
+
     def tiled_encode(self, x: torch.FloatTensor, return_dict: bool = True) -> VQEncoderOutput:
         r"""Encode a batch of images using a tiled encoder.
 
